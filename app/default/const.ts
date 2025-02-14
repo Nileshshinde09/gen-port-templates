311fedf267668d887b00b526e96a12fd965eadbc
Nileshshinde09 <nileshshindeofficial@gmail.com>
2025-02-14 19:36:11 +0530

    initial commit

--- app/default/const.ts
+++
@@ -1,5 +0,0 @@
-const REPOUSERNAME = "Nileshshinde09";
-const REPONAME = "gen-port";
-const VERSION = "v1.0.0";
-const PORTFOLIO_NAME = "portfolio-01";
-export const completeURL = `https://github.com/${REPOUSERNAME}/${REPONAME}/tree/main/client/src/www/app/default/${VERSION}/${PORTFOLIO_NAME}`;
