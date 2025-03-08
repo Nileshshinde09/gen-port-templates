8ba6cbb72a1f0b57d27448a136d3fb9caf0c8a75
Nileshshinde09 <nileshshindeofficial@gmail.com>
2025-03-08 21:38:04 +0530

    code added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import React from "react";
-import { Navbar } from "./components";
 import { Home } from "./pages";
 const App = (): React.ReactNode => {
   return (
       <main className="w-full h-full">
-        <Navbar />
         <Home/>
       </main>
   );
