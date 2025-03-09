bf1a450fe322095b5be60d936e52cd7cf3ad6dee
Nileshshinde09 <nileshshindeofficial@gmail.com>
2025-03-09 22:20:48 +0530

    new code added

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,22 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
-
+// https://vitejs.dev/config/
 export default defineConfig({
+  server:{
+    host: "localhost",
+    port: 5173,
+    proxy:{
+      "/api":"http://localhost:8000"
+    }
+  },
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  define:{
+    global:{},
+  }
 })
